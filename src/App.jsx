6a47b5bfb0bf90f98dee7930e9d5200dcc52cbf3
Neelumishra <neelumishra0397@gmail.com>
2023-04-16 22:26:18 +0530

    Header Task done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
 import Sidebar from "./component/Sidebar";
+import Header from "./component/Header";
 
 function App(){
   return <div className="App">
+    <Header/>
     <Sidebar/>
   </div>;
 }
