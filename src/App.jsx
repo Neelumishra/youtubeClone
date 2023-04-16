34d26642c173d1eff9bb2b763e2fdba2f28f6392
Parvat1.Singh <Parvat1.Singh@ril.com>
2023-04-16 23:56:28 +0530

    added sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import React from "react";
 import "./App.css";
 import Sidebar from "./component/Sidebar";
 import Header from "./component/Header";
 
 function App(){
+  const [sidebarOpen,setSidebarOpen] = React.useState(false)
+  const handleSidebar = () =>{
+    setSidebarOpen(!sidebarOpen)
+  } 
   return <div className="App">
-    <Header/>
-    <Sidebar/>
+    <Header handleSidebar={handleSidebar}/>
+    <Sidebar sidebarOpen={sidebarOpen}/>
   </div>;
 }
 
--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,7 +1,56 @@
 import React from 'react'
+import { AiOutlineHome, AiOutlineHistory, AiOutlineLike } from "react-icons/Ai";
+import { SiYoutubemusic } from "react-icons/Si";
+import {
+  MdSubscriptions,
+  MdVideoLibrary,
+  MdOndemandVideo,
+} from "react-icons/Md";
+import { BsClockHistory } from "react-icons/Bs"
 
-export default function Sidebar() {
+
+export default function Sidebar({ sidebarOpen }) {
   return (
-    <div>Sidebar</div>
-  )
+    <>
+      {sidebarOpen && (
+        <div className="sidebar bg-dark text-white p-1">
+          <ul type="none">
+            <li>
+              <AiOutlineHome className="me-3" />
+              <strong>Home</strong>
+            </li>
+            <li>
+              <SiYoutubemusic className="me-3" />
+              <strong>Shorts</strong>
+            </li>
+            <li>
+              <MdSubscriptions className="me-3" />
+              <strong>Subscription</strong>
+            </li>
+            <hr />
+            <li>
+              <MdVideoLibrary className="me-3" />
+              <strong>Libary</strong>
+            </li>
+            <li>
+              <AiOutlineHistory className="me-3" />
+              <strong>History</strong>
+            </li>
+            <li>
+              <MdOndemandVideo className="me-3" />
+              <strong>Your Vidoe</strong>
+            </li>
+            <li>
+              <BsClockHistory className="me-3" />
+              <strong>Watch Later</strong>
+            </li>
+            <li>
+              <AiOutlineLike className="me-3" />
+              <strong>Liked video</strong>
+            </li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
 }
